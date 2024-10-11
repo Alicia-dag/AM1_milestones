0469b2d42d8c881b821e6c5e11ffb70d2b1f1d21
Alicia-dag <agg9agg@gmail.com>
2024-10-11 14:11:23 +0200

    Milestone 1 y comienzo del 2

--- My milestones/Milestone2.py
+++ My milestones/Milestone2.py
@@ -2,6 +2,24 @@ from numpy import array, zeros, linspace, concatenate
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
 
+
+#################################################### DATOS ########################################################
+# Condiciones iniciales
+x0 = 1.0
+y0 = 0.0
+vx0 = 0.0
+vy0 = 1.0
+
+# Instantes inicial, final y paso del tiempo
+t0 = 0.0
+tf = 10.0
+dt = 0.01
+
+# Número de intervalos (= nº de instantes de tiempo - 1)
+N = int((tf - t0) / dt)
+
+
+################################################# FUNCIONES #######################################################
 # KEPLER
 def Kepler(U): #U = vector
     r = U[0:2]
@@ -25,7 +43,6 @@ def Euler (F, U, dt, t):
 #           Partición temporal
 # Output:
 #           Solución para todo instante temporal y para todo componente
-
 def Cauchy(Esquema, F, U0, t): 
     N = len(t)-1  # Por como se empieza a contar en Python
     U = zeros((N+1,len(U0)))
@@ -44,12 +61,23 @@ def RK4 (F, U, dt, t):
     return
 
 
+################################################### CÓDIGO ########################################################
+# Inicializamos el vector de intantes temporales en los que se calcula la solución
+t_values = linspace(t0, tf, N)
+n = len(t_values)
 
-###### CÓDIGO ######
+# Vector de condiciones iniciales
+U0 = array ([x0, y0, vx0, vy0])
 
+# Matrices de soluciones
+U_Euler = zeros((n,4))
+U_Euler[0] = U0 
+F_Euler = zeros(4)
+
+
+# Llamadas
 F_Euler[n,:] = Kepler(U_Euler[n,:])
-
-U_Euler[n+1,:] = U_Euler[n,:] + (t[n+1]-t[n]) * F_Euler[n,:]
+U_Euler[n+1,:] = U_Euler[n,:] + dt * F_Euler[n,:]
 
 
 # Simplifica las dos líneas anteriores en una
@@ -61,3 +89,18 @@ U_Euler = Cauchy (Euler, Kepler, U0, t)
 
 # Aplicando el RK4
 U_Euler = Cauchy (Euler, Kepler, U0, t)
+
+
+
+################################################# GRÁFICAS #########################################################
+# Gráficas de las soluciones
+plt.figure(figsize=(10, 6))
+plt.plot(U_Euler[:, 0], U_Euler[:, 1], label="Euler Explícito", alpha=0.6)
+plt.plot(U_RK4[:, 0], U_RK4[:, 1], label="Runge-Kutta 4 etapas", alpha=0.6)
+plt.plot(U_CN[:, 0], U_CN[:, 1], label="Crank-Nickolson", alpha=0.6)
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Solución de la EDO usando diferentes métodos numéricos")
+plt.grid()
+plt.show()
